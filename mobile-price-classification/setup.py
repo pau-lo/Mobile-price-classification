b65b77bd1894265c520ad8d7ba6f61f140a392e6
pau-lo <pau_lo_lop_ez@protonmail.com>
2021-03-30 15:26:50 -0700

    Added requirements.txt file

--- mobile-price-classification/setup.py
+++ mobile-price-classification/setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
+    description='Using autosklearn to predict mobile phone prices.',
+    author='Paulo Lopez',
     license='MIT',
 )
