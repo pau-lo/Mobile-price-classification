3bcfb7fa279395c52d5479a552c9fdd0588dd0e1
pau-lo <pau_lo_lop_ez@protonmail.com>
2021-03-31 14:37:30 -0700

    added notebook for initial eda.

--- mobile-price-classification/setup.py
+++ mobile-price-classification/setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
+
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Using autosklearn to predict mobile phone prices.',
-    author='Paulo Lopez',
-    license='MIT',
+    version="0.1.0",
+    description="Using autosklearn to predict mobile phone prices.",
+    author="Paulo Lopez",
+    license="MIT",
 )
